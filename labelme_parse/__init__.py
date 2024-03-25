8ff6af95a9e7a4b93e411112dea86a755018269a
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-03-25 12:09:47 +0545

    refactor: change project name

---
+++ labelme_parse/__init__.py
@@ -0,0 +1,17 @@
+from labelme_parse.labels import get_point
+from labelme_parse.labels import get_point_names
+from labelme_parse.labels import get_poly
+from labelme_parse.labels import get_poly_names
+from labelme_parse.labels import get_rect
+from labelme_parse.labels import get_rect_names
+from labelme_parse.labels import get_rect_relative
+
+__all__ = [
+    "get_point",
+    "get_point_names",
+    "get_poly",
+    "get_poly_names",
+    "get_rect",
+    "get_rect_names",
+    "get_rect_relative",
+]
